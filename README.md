e6258543f3c240427f863e892295a51acd33090f
graffhyrum <69610278+graffhyrum@users.noreply.github.com>
2022-10-11 16:49:44 -0500

    "version": "0.0.4" acceptable v1 of main gui css
    Signed-off-by: Pendragon <69610278+graffhyrum@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Tauri + Vanilla - Bar Runner
+
 Toy project to create a desktop app for making and editing text RP templates
+
+[https://tauri.app/](https://tauri.app/)
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,26 +3,26 @@
     windows_subsystem = "windows"
 )]
 
-use br_tauri_hcj::{FileNames, RpMessage, Section, Snippet};
+// use br_tauri_hcj::{FileNames, RpMessage, Section, Snippet};
 
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-// #[tauri::command]
-// fn greet(name: &str) -> String {
-//     format!("Hello, {}! You've been greeted from Rust!", name)
-// }
 #[tauri::command]
-fn search(search_str: &str) -> Result<RpMessage,String> {
-    //check for directory with JSONs
-    //create if it doesn't exist
-    //check for JSONs
-    //create if they dont exist
-    //read elements to Vectors
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
+// #[tauri::command]
+// fn search(search_str: &str) -> Result<RpMessage,String> {
+//     //check for directory with JSONs
+//     //create if it doesn't exist
+//     //check for JSONs
+//     //create if they dont exist
+//     //read elements to Vectors
+// }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![search])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+      .invoke_handler(tauri::generate_handler![greet])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
